0ec67830898159d1cd18b692d50413f89678061e
naila-saleh <nailasaleh2004@gmail.com>
2024-06-06 21:29:36 +0300

    change size from js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-document.querySelector(".userProfile h2").textContent="hello naila";
-document.querySelector(".userInfo p").classList.add("nn");
+var body=document.querySelector('body');
+var increse=document.querySelector(".increaseFontBtn");
+var decrese=document.querySelector(".decreaseFontBtn");
+function increaseFontSize(){
+    body.classList.add("increaseFont");
+    body.classList.remove("decreaseFont");
+}
+function decreaseFontSize(){
+    body.classList.add("decreaseFont");
+    body.classList.remove("increaseFont");
+}
+decrese.onclick=decreaseFontSize;
+increse.onclick=increaseFontSize;
