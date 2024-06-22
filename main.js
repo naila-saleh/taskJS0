80407267f57892437f81d8ce514386263408c99d
naila-saleh <nailasaleh2004@gmail.com>
2024-06-22 12:40:45 +0300

    add li by js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-var body=document.querySelector('body');
-var increse=document.querySelector(".increaseFontBtn");
-var decrese=document.querySelector(".decreaseFontBtn");
-function increaseFontSize(){
-    body.classList.add("increaseFont");
-    body.classList.remove("decreaseFont");
-}
-function decreaseFontSize(){
-    body.classList.add("decreaseFont");
-    body.classList.remove("increaseFont");
-}
-decrese.onclick=decreaseFontSize;
-increse.onclick=increaseFontSize;
+var data="";
+data += "<li>c++</li>";
+data += "<li>Java</li>";
+data += "<li>JS</li>";
+document.querySelector("ul").innerHTML= data;
