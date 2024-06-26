fefa5cc3be0abeca04b54c6fff4af7a62a444edc
naila-saleh <nailasaleh2004@gmail.com>
2024-06-26 23:02:50 +0300

    exchangeMoneyByJS

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-var data="";
-data += "<li>c++</li>";
-data += "<li>Java</li>";
-data += "<li>JS</li>";
-document.querySelector("ul").innerHTML= data;
+var exchangeForm=document.querySelector(".exchangeForm");
+var exchanges={};
+exchangeForm.onsubmit=function (e){
+    e.preventDefault();
+    var elements=e.target.elements;
+    exchanges={
+        amount:elements[0].value,
+        exchange:elements[1].value
+    }
+    printPriceExchange();
+}
+function printPriceExchange(){
+    var data=``;
+    var afterExchange=exchanges.amount;
+    if(exchanges.exchange==="dollar")afterExchange=exchanges.amount*3.5;
+    else if(exchanges.exchange==="dinar")afterExchange=exchanges.amount*5;
+    data+=`
+            <p class="result">The result after exchange : ${afterExchange}</p>
+    `
+    document.querySelector(".result").innerHTML=data;
+}
